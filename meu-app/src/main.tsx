ae784fd05305d048c9a9a91bedfe308354d5a1f8
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-07-16 20:56:44 +0000

    git

--- meu-app/src/main.tsx
+++ meu-app/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react' // StrictMode: ferramenta de desenvolvimento para encontrar problemas e bugs no código antes de eles irem para produção.
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
